0f6e253faf2b92b4f03081fd05d15bb7d4bf2177
luismi4157 <luismi4157@gmail.com>
2024-01-02 14:57:35 -0700

    Chapter 4

--- WiredBrainCoffee.CustomersApp/App.xaml.cs
+++ WiredBrainCoffee.CustomersApp/App.xaml.cs
@@ -6,12 +6,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace WiredBrainCoffee
+namespace WiredBrainCoffee.CustomersApp
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
-    {
-    }
+  /// <summary>
+  /// Interaction logic for App.xaml
+  /// </summary>
+  public partial class App : Application
+  {
+  }
 }
