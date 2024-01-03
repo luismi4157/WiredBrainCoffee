ccffc068f2e7f67b07172c27eccf8273eb4d714b
luismi4157 <luismi4157@gmail.com>
2024-01-03 16:02:45 -0700

    Chapter 5 End

--- WiredBrainCoffee.CustomersApp/MainWindow.xaml.cs
+++ WiredBrainCoffee.CustomersApp/MainWindow.xaml.cs
@@ -1,41 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace WiredBrainCoffee.CustomersApp
 {
-  /// <summary>
-  /// Interaction logic for MainWindow.xaml
-  /// </summary>
-  public partial class MainWindow : Window
-  {
-    public MainWindow()
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
     {
-      InitializeComponent();
-    }
-
-        private void ButtonMoveNavigation_Click(object sender, RoutedEventArgs e)
+        public MainWindow()
         {
-            //var column = (int)customerListGrid.GetValue(Grid.ColumnProperty);
-
-            //var newColumn = column == 0 ? 2 : 0;
-            //customerListGrid.SetValue(Grid.ColumnProperty, newColumn);
-
-            var column = Grid.GetColumn(customerListGrid);
-
-            var newColumn = column == 0 ? 2 : 0;
-            Grid.SetColumn(customerListGrid, newColumn);
+            InitializeComponent();
         }
     }
 }
