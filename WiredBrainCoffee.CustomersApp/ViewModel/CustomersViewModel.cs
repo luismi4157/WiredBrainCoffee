4d0ffd9af41abbb149517737c31d614278fae8c2
luismi4157 <luismi4157@gmail.com>
2024-01-05 11:00:02 -0700

    Chapter 7 Begin

--- WiredBrainCoffee.CustomersApp/ViewModel/CustomersViewModel.cs
+++ WiredBrainCoffee.CustomersApp/ViewModel/CustomersViewModel.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.ObjectModel;
-using System.Data.Common;
+﻿using System.Collections.ObjectModel;
 using System.Linq;
 using System.Threading.Tasks;
 using WiredBrainCoffee.CustomersApp.Data;
@@ -8,72 +6,75 @@ using WiredBrainCoffee.CustomersApp.Model;
 
 namespace WiredBrainCoffee.CustomersApp.ViewModel
 {
-    public class CustomersViewModel : ViewModelBase
+  public class CustomersViewModel : ViewModelBase
+  {
+    private readonly ICustomerDataProvider _customerDataProvider;
+    private CustomerItemViewModel? _selectedCustomer;
+    private NavigationSide _navigationSide;
+
+    public CustomersViewModel(ICustomerDataProvider customerDataProvider)
     {
-        private readonly ICustomerDataProvider _customerDataProvider;
-        private CustmerItemViewModel? _selectedCustomer;
-        private NavigationSide _navigationColumnSide;
-
-        public CustomersViewModel(ICustomerDataProvider customerDataProvider)
-        {
-            _customerDataProvider = customerDataProvider;
-        }
-
-        public ObservableCollection<CustmerItemViewModel> Customers { get; } = new();
-
-        public CustmerItemViewModel? SelectedCustomer
-        {
-            get => _selectedCustomer;
-            set
-            {
-                _selectedCustomer = value;
-                RaisePropertyChanged();
-            }
-        }
-
-        public NavigationSide NavigationColumnSide
-        {
-            get => _navigationColumnSide;
-            private set
-            {
-                _navigationColumnSide = value;
-                RaisePropertyChanged();
-            }
-        }
-
-        public async Task LoadAsync()
-        {
-            if (Customers.Any()) return;
-
-            var customers = await _customerDataProvider.GetAllAsync();
-            if (customers is not null)
-            {
-                foreach (var customer in customers)
-                {
-                    Customers.Add(new CustmerItemViewModel(customer));
-                }
-            }
-        }
-
-        internal void Add()
-        {
-            var customer = new Customer { FirstName = "New" };
-            var viewModel = new CustmerItemViewModel(customer);
-            Customers.Add(viewModel);
-            SelectedCustomer = viewModel;
-        }
-
-        internal void MoveNavigation()
-        {
-            NavigationColumnSide = NavigationColumnSide == NavigationSide.Left 
-                ? NavigationSide.Right 
-                : NavigationSide.Left;
-        }
-
-        public enum NavigationSide
-        {
-            Left,
-            Right
-        }
+      _customerDataProvider = customerDataProvider;
     }
+
+    public ObservableCollection<CustomerItemViewModel> Customers { get; } = new();
+
+    public CustomerItemViewModel? SelectedCustomer
+    {
+      get => _selectedCustomer;
+      set
+      {
+        _selectedCustomer = value;
+        RaisePropertyChanged();
+      }
+    }
+
+    public NavigationSide NavigationSide
+    {
+      get => _navigationSide;
+      private set
+      {
+        _navigationSide = value;
+        RaisePropertyChanged();
+      }
+    }
+
+    public async Task LoadAsync()
+    {
+      if (Customers.Any())
+      {
+        return;
+      }
+
+      var customers = await _customerDataProvider.GetAllAsync();
+      if (customers is not null)
+      {
+        foreach (var customer in customers)
+        {
+          Customers.Add(new CustomerItemViewModel(customer));
+        }
+      }
+    }
+
+    internal void Add()
+    {
+      var customer = new Customer { FirstName = "New" };
+      var viewModel = new CustomerItemViewModel(customer);
+      Customers.Add(viewModel);
+      SelectedCustomer = viewModel;
+    }
+
+    internal void MoveNavigation()
+    {
+      NavigationSide = NavigationSide == NavigationSide.Left
+        ? NavigationSide.Right
+        : NavigationSide.Left;
+    }
+  }
+
+  public enum NavigationSide
+  {
+    Left,
+    Right
+  }
 }
--- WiredBrainCoffee.CustomersApp/ViewModel/ViewModelBase.cs
+++ WiredBrainCoffee.CustomersApp/ViewModel/ViewModelBase.cs
@@ -3,13 +3,13 @@ using System.Runtime.CompilerServices;
 
 namespace WiredBrainCoffee.CustomersApp.ViewModel
 {
-    public class ViewModelBase : INotifyPropertyChanged
-    {
-        public event PropertyChangedEventHandler? PropertyChanged;
+  public class ViewModelBase : INotifyPropertyChanged
+  {
+    public event PropertyChangedEventHandler? PropertyChanged;
 
-        protected virtual void RaisePropertyChanged([CallerMemberName] string? propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
+    protected virtual void RaisePropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+      PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
+  }
 }
